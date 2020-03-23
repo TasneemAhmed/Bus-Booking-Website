26b29cdbc0fb645ed2c6dd3eb21e688e5e1c2056
Ayabdelrahman <33642209+20170132@users.noreply.github.com>
2020-03-23 23:13:02 +0200

    Bus Model and its view and controller is created

---
+++ BusBookingSystem/Migrations/202003232022484_businitial.cs
@@ -0,0 +1,28 @@
+﻿namespace BusBookingSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class businitial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Buses",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        MLicensePlateNo = c.String(nullable: false, maxLength: 5),
+                        MBusCapacity = c.Int(nullable: false),
+                        MBusType = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Buses");
+        }
+    }
+}
