8881633ec0367db21fca4c1096433b5f95adefbb
engy34 <62096930+engy34@users.noreply.github.com>
2020-06-03 17:17:57 -0600

    make warning message when the feedback not entered

--- BusBookingSystem/Controllers/HomeController.cs
+++ BusBookingSystem/Controllers/HomeController.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+
 using System.Web.UI.WebControls;
 using BusBookingSystem.Models;
 using System.Text;
+
 using System.Net;
 using System.Net.Mail;
+
 
 namespace BusBookingSystem.Controllers
 {
@@ -33,45 +35,54 @@ namespace BusBookingSystem.Controllers
             return View();
         }
         [HttpGet]
-        public ViewResult Feedback()
+        public ActionResult Feedback()
         {
-            return View(); 
+
+
+            return View();
         }
-
         [HttpPost]
-
-        public ViewResult Feedback(Feedback fb)
+        public ActionResult Feedback(Feedback fb)
         {
-
-            if (ModelState.IsValid)
+            try
             {
-                MailMessage mail = new MailMessage();
-                mail.To.Add("busbookinga@gmail.com");
-                mail.From = new MailAddress(fb.EMail);
-                mail.Subject = fb.Subject;
+                if (ModelState.IsValid)
+                {
+                    MailMessage mail = new MailMessage();
+                    mail.To.Add("busbookinga@gmail.com");
+                    mail.From = new MailAddress(fb.EMail);
+                    mail.Subject = fb.Subject;
 
-                StringBuilder Body = new StringBuilder();
-                Body.Append("Name   :   " + fb.name);
-                Body.Append(" \n Email  :   " + fb.EMail);
-                Body.Append(" \n Message :   " + fb.text);
+                    StringBuilder Body = new StringBuilder();
+                    Body.Append("Name   :   " + fb.name);
+                    Body.Append(" \n Email  :   " + fb.EMail);
+                    Body.Append(" \n Message :   " + fb.text);
 
-                mail.Body = Body.ToString();
-                mail.IsBodyHtml = true;
-                SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new System.Net.NetworkCredential
-                ("busbookinga@gmail.com", "BusBookingSystem2020");// Enter seders User name and password
-                smtp.EnableSsl = true;
-                smtp.Send(mail);
-                return View("Feedback", fb);
+                    mail.Body = Body.ToString();
+                    mail.IsBodyHtml = true;
+                    SmtpClient smtp = new SmtpClient();
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("busbookinga@gmail.com", "BusBookingSystem2020");// Enter seders User name and password
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Thank you for your feedback";
+                    return View();
+
+                }
+                else
+                {
+                    return View();
+                }
             }
-            else
+            catch (Exception e)
             {
+                ViewBag.Message = "There is an error !! please resend your feedback";
                 return View();
             }
-
         }
+
     }
 }
